af8bee102a95cb25735c44a4ca2917abebc68ef2
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2024-07-19 20:17:26 -0300

    Gson da google como dependência e iniciando o sistema para popular o banco

---
+++ src/main/java/br/com/pokedexspring/pokedex/Seed.java
@@ -0,0 +1,26 @@
+package br.com.pokedexspring.pokedex;
+
+import br.com.pokedexspring.pokedex.providers.Requests;
+
+public class Seed {
+  public static void main(String[] args) {
+
+    Requests requests = new Requests();
+
+    for (int i = 1; i <= 2 ; i++) {
+
+      //buscar os dados do pokémon
+
+
+      //buscar spécies do pokémon, é assim que definiram
+
+
+      //buscar a corrente de evoluções
+
+      //salvar em banco
+
+
+    }
+
+  }
+}
---
+++ src/main/java/br/com/pokedexspring/pokedex/providers/Requests.java
@@ -0,0 +1,32 @@
+package br.com.pokedexspring.pokedex.providers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+import java.util.Optional;
+
+public class Requests {
+
+  public enum HttpMethod {
+    GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, TRACE
+  }
+
+  public HttpResponse<String> sendRequest(String url, HttpMethod method, Optional<Map<String, String>> headers, Optional<String> body) throws Exception {
+    try (HttpClient client = HttpClient.newHttpClient()) {
+
+      HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+          .uri(URI.create(url))
+          .method(method.toString(), body.map(HttpRequest.BodyPublishers::ofString)
+              .orElse(HttpRequest.BodyPublishers.noBody()));
+
+      headers.ifPresent(h -> h.forEach(requestBuilder::header));
+
+      HttpRequest request = requestBuilder.build();
+
+      return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+  }
+}
