940c3af9b98bd25171f7bfafe406ae6045275028
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2024-07-20 15:54:12 -0300

    Correção do método de busca, o jpa não havia entendido o que eu queria

--- src/main/java/br/com/pokedexspring/pokedex/modules/abilities/repository/IAbilitiesRepository.java
+++ src/main/java/br/com/pokedexspring/pokedex/modules/abilities/repository/IAbilitiesRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface IAbilitiesRepository extends JpaRepository<Abilities, UUID> {
-  List<Abilities> findManyWhereDeletedAtIsNull();
+  List<Abilities> findByDeletedAtIsNull();
 }
